02443be9fdab66898995c60b7da9123d7c6e2382
27fx <feix517@gmail.com>
2024-04-22 20:06:10 +0800

    菜品分页查询完成

--- sky-server/src/main/java/com/sky/controller/admin/CommonController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -6,13 +6,18 @@ package com.sky.controller.admin;
 
 
 import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
 
 @RestController
 @Slf4j
@@ -20,6 +25,10 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Api(tags="通用接口")
 public class CommonController {
+
+
+    @Autowired
+    private AliOssUtil aliOssUtil;
 
 //文件上传
 
@@ -30,7 +39,30 @@ public class CommonController {
 
 
         log.info("文件上传,{}",file);
-        return null;
+
+        try {
+
+            String originalFilename=file.getOriginalFilename();
+
+
+            String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+            String objectName = UUID.randomUUID().toString() + extension;
+
+            String FilePath=aliOssUtil.upload(file.getBytes(),objectName);
+            return Result.success(FilePath);
+
+
+        } catch (IOException e) {
+
+            log.error("文件上传失败，{}",e);
+
+        }
+
+
+        return Result.error("文件上传失败");
+
+
+
 
     }
 
