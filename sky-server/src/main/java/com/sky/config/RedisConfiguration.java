4e7cfebc795bb793b2eefae342b78a5440e543b1
27fx <feix517@gmail.com>
2024-04-24 18:18:20 +0800

    店铺营业设置功能业务开发

---
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -0,0 +1,35 @@
+package com.sky.config;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.stereotype.Controller;
+
+@Configuration
+
+@Slf4j
+public class RedisConfiguration {
+
+
+    @Bean
+
+    public RedisTemplate  redisTemplate (RedisConnectionFactory redisConnectionFactory){
+
+
+        log.info("开始创建redis模板对象...");
+        RedisTemplate redisTemplate = new RedisTemplate();
+
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        return redisTemplate;
+    }
+
+
+
+}
